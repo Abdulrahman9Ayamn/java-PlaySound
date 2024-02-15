649331dae2dae28c1113313233e8bee1fc5b0fc3
Abdulrhman <body1999221@gmail.com>
2024-02-15 23:18:51 +0200

    Createed the main Project Structure

---
+++ src/main/java/com/mycompany/playsound/PlaySound.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.playsound;
+
+/**
+ *
+ * @author qwertet
+ */
+public class PlaySound {
+
+    public static void main(String[] args) {
+        new formSound().setVisible(true);
+       
+
+    }
+}
